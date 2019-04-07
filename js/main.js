5d6070d80d954ccc07470333012d6b812675a6b1
RG3 <42303583+KirVik@users.noreply.github.com>
2019-04-07 15:35:41 +0300

    hhhhhhhhh

--- js/main.js
+++ js/main.js
@@ -8,7 +8,41 @@ $(function (){
         prevArrow: false,
         nextArrow: false,
     });
-    
+    $('.product-photo__slader').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/product-foto/rewind-left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/product-foto/rewind-right.png"></button>',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    })
     // гамбургер
     $('.menu-open').on("click", function (event) {
         event.stopPropagation();
